14cdee2788321637bb54b3737927ed1bfcaa02df
Cedrus81 <erez.eitan.97@gmail.com>
2022-10-17 16:31:11 +0300

    Added a carousel for the header

---
+++ js/services/i18n.js
@@ -0,0 +1,19 @@
+'use strict'
+
+var gTranslate = {
+    specialities: {
+        eng: 'Specialities',
+        esp: 'especialidades',
+        he: 'התמחויות'
+    },
+    'specialities-desc': {
+        eng: 'These are the technologies I can use for now:',
+        esp: 'Estas son las tecnologías que utilizo:',
+        he: ':אלו הכלים בהם אני יודע לעבוד, נכון לעכשיו'
+    },
+    'javascript-desc': {
+        eng: '',
+        esp: '',
+        he: ''
+    }
+}
